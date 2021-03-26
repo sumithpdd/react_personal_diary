0c7519ea2057c00cabe71a0fe862f2de0034117c
Sumith Damodaran <sumithpd@gmail.com>
2021-03-26 09:48:19 +0100

    Creating the App Components

--- personal-diary-frontend/src/App.js
+++ personal-diary-frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
 import './App.css';
-
 function App() {
+  const [currentUser, setCurrentUser] = useState(undefined);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login">
+          <Login setCurrentUser={setCurrentUser} />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
-
 export default App;
---
+++ personal-diary-frontend/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+const Home = () => {
+    return (
+        <section className="hero is-success is-fullheight">
+            <div className="hero-body">
+                <div className="container">
+                    <h1 className="title is-1">
+                        Welcome to your personal diary!
+                    </h1>
+                    <h2 className="subtitle is-5">
+                        Write down and manage your notes with ease.
+                    </h2>
+                </div>
+            </div>
+        </section>
+    );
+}; export default Home;
